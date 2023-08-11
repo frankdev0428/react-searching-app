a4e21feda93f8c098cb0a0090f13bb24f94ce174
frankdev0428@gmail.com <frankdev0428@gmail.com>
2023-08-10 21:35:13 -0700

    react-hook-form modify

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -9,41 +9,38 @@ const Form = () => {
   return (
     <form onSubmit={handleSubmit(onSubmit)} className='form-container'>
      <div className="mb-3">
-        <label htmlFor="name" className="form-label">Frist Name
+        <label htmlFor="first-Name" className="form-label">First Name
      </label>
-     <input {...register('name',{ required: true, minLength: 3})} 
-      id="name" type="text" className="form-control" />
-      
+     <input {...register('firstName',{ required: true, minLength: 3})} 
+      id="first-Name" type="text" className="form-control" />
      </div>
 
      <div className="mb-3">
-        <label htmlFor="name" className="form-label">Last Name
+        <label htmlFor="last-name" className="form-label">Last Name
      </label>
-     <input {...register('name',{ required: true, minLength: 3})} 
-      id="name" type="text" className="form-control" />
+     <input {...register('lastName',{ required: true})} 
+      id="last-name" type="text" className="form-control" />
      </div>
       <div className="mb-3">
-         <label htmlFor="" className="form-label">Phone Number</label>
-         <input type="number" className="form-control" />
+         <label htmlFor="phone-number" className="form-label">Phone Number</label>
+         <input {...register('phoneNumber',{required:true, minLength: 9})} id='phone-number' type="number" className="form-control" />
       </div>
-
       <div className="mb-3">
-         <label htmlFor="" className="form-label">Email</label>
-         <input type="email" className="form-control" />
+         <label htmlFor="email" className="form-label">Email</label>
+         <input {...register('email',{required: true})} id='email' type="email" className="form-control" />
       </div>
-   
      <div className="mb-3">
         <label htmlFor="age" className="form-label">Age</label>
         <input  {...register('age')}  id="age" type="number" className="form-control" required/>
      </div>
         
       <div className="mb-3">
-      <label htmlFor="" className="form-label">Password</label>
-      <input type="password" className="form-control"/>
+      <label htmlFor="password" className="form-label">Password</label>
+      <input {...register('password', {required: true})} id='password' type="password" className="form-control"/>
       </div>
       <div className="mb-3">
-         <label htmlFor="" className="form-label">Confirm Password</label>
-         <input type="password" className="form-control" />
+         <label htmlFor="confirm-password" className="form-label">Confirm Password</label>
+         <input {...register('confirmPassword', {required: true})} id='confirm-password' type="password" className="form-control" />
       </div>
         <button className="btn btn-primary signup-but" type='submit'>Sign Up</button>
     </form>
